0dd7b5b011922e301c6dbe1bba9f8b68838afac7
LiamWu <lovan0616@gmail.com>
2021-01-22 20:53:57 +0800

    feat: add Event.vue

---
+++ src/components/Album.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="['album-wrapper', 'my-5', event.name ]" :id="event.name">
+    <h1 class="font-weight-light">{{ event.name_ch }}</h1>
+    <v-chip class="dateChip red white--text">
+      <v-icon left class="white--text">mdi-calendar-minus</v-icon>
+      {{ event.date }}
+    </v-chip>
+    <v-carousel :height="{100: windowWidth < 600 }">
+      <v-carousel-item v-for="pic in event.album" :key="pic" :src="pic">
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Album',
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      windowWidth: window.innerWidth
+    }
+  }
+}
+</script>
+
+<style scoped>
+.album-wrapper {
+  position: relative;
+}
+
+.dateChip {
+  position: absolute;
+  top: 8px;
+  right: 0;
+}
+
+
+</style>
--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,5 +1,50 @@
 <template>
   <div class="event">
-    <h1>This is an event page</h1>
+    <v-container>
+      <div id="tabs"></div>
+      <div class="albums">
+        <Album
+          v-for="event in events"
+          :key="event.name"
+          :event="event"
+         />
+      </div>
+      <div>
+      </div>
+    </v-container>
   </div>
 </template>
+
+<script>
+import Album from '../components/Album'
+export default {
+  name: 'Event',
+  components: {
+    Album
+  },
+  data() {
+    return {
+      events: [
+        {
+          name: 'speech',
+          name_ch: '校園演講',
+          date: '2015/4/8-10',
+          album: ['/speech/pic1.png', '/speech/pic2.png', '/speech/pic3.png', '/speech/pic4.png', '/speech/pic5.png']
+        },
+        {
+          name: 'sandwich',
+          name_ch: '手作三明治',
+          date: '2015/3/26',
+          album: ['/sandwich/pic1.png', '/sandwich/pic2.png', '/sandwich/pic3.png', '/sandwich/pic4.png', '/sandwich/pic5.png', '/sandwich/pic6.png']
+        },
+        {
+          name: 'dinner',
+          name_ch: '100份便當',
+          date: '2014/12/30',
+          album: ['/dinner/pic1.png', '/dinner/pic2.png', '/dinner/pic3.png', '/dinner/pic4.png']
+        },
+      ]
+    }
+  }
+}
+</script>
